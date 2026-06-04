b8530eab9cd6f0ceafb8f03152971bb2d85e48dc
Mandeep Reddy <your_github_email@gmail.com>
2026-06-04 17:16:51 +0530

    Fix email SMTP port

--- backend/src/utils/emailService.js
+++ backend/src/utils/emailService.js
@@ -13,7 +13,9 @@ const createTransporter = () => {
   }
 
   return nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
       user: user,
       pass: pass
